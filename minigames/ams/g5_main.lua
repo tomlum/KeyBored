4d07a24a04504e2bce13e843d3a38320d375c1fb
tomlum <checkeredlines@gmail.com>
2016-10-27 14:00:06 -0400

    Dope progress on All My Suns!!!!

--- minigames/ams/g5_main.lua
+++ minigames/ams/g5_main.lua
@@ -1,5 +1,20 @@
---Move force is affected by all planets?  or just those pulling?
+--Even though it's programming and robotics, Our goal isn't quantitative to build 6 robots that are 
+--On the side of both learning
+--On the pedagogical learning
+--Even though it's robots and programming, this is a very qualitative academic
+--Our goal isn't to 
 
+--CROSS TALK, I held office hours
+
+--John said it and I didn't believe him
+
+
+
+
+
+
+--Move force is affected by all planets?  or just those pulling?
+--MAybe don't make orbit based on size?  make it fixed?  So you don't have these gigantic orbits
 
 function g5NewImage(filePath)
 	return lg.newImage("minigames/ams/"..filePath)
@@ -11,6 +26,7 @@ end
 
 im_moon = g5NewImage("moon.png")
 im_sun = g5NewImage("sun.png")
+im_carona = g5NewImage("carona.png")
 G = 70
 S = 10
 realG = false
@@ -22,15 +38,17 @@ real = false
 drawForces = false
 speed = 2
 gravspeed = 400000
+criticalMass = 4
+explosionForce = 1
 
 function g5_load()
 	drawForcess = true
 	planets = {}
 	suns = {}
-	table.insert(suns, {num = 1, key = "q", x=300, y = height*1/4, lr = 1, size = 1})
-	table.insert(suns, {num = 2, key = "z", x=300, y = height*3/4, lr = 1, size = 1})
-	table.insert(suns, {num = 3, key = "p", x=width-300, y = height*1/4, lr = -1, size = 1})
-	table.insert(suns, {num = 4, key = "m", x=width-300, y = height*3/4, lr = -1, size = 1})
+	table.insert(suns, {num = 1, key = "q", x=300, y = height*1/4, lr = 1, size = 1, speed = 1, spin = 0})
+	table.insert(suns, {num = 2, key = "z", x=300, y = height*3/4, lr = 1, size = 1, speed = 1, spin = 0})
+	table.insert(suns, {num = 3, key = "p", x=width-300, y = height*1/4, lr = -1, size = 1, speed = 1, spin = 0})
+	table.insert(suns, {num = 4, key = "m", x=width-300, y = height*3/4, lr = -1, size = 1, speed = 1, spin = 0})
 
 	time = 0
 
@@ -48,7 +66,7 @@ function g5_load()
 	function updatePlanets()
 		for i,planet in ipairs(planets) do
 			sundis = -1
-			planet.mysun = suns[0]
+			planet.mysun = suns[1]
 			for j,sun in ipairs(suns) do
 				thisSunDis = cu.dis(planet.x, planet.y, sun.x, sun.y)/sun.size
 				if sundis == -1 or thisSunDis < sundis then
@@ -122,7 +140,10 @@ function g5_load()
 				end
 
 				local trailscale = planet.size-((trailduration-tra.time)/trailduration)*planet.size
-				lg.setColor(255,255,255,((tra.time)/trailduration)*50)
+				if planet.mysun ~= nil then
+					local color = playerColors[planet.mysun.num+1]
+					lg.setColor(color[1], color[2], color[3],((tra.time)/trailduration)*15)
+				end
 				lg.draw(planet.im, tra.x, tra.y, 0, trailscale, trailscale, planet.center.x, planet.center.y)
 			end
 
@@ -164,27 +185,45 @@ end
 function g5_update()
 	updatePlanets()
 	for i,sun in ipairs(suns) do
+		if sun.speed > criticalMass and not sun.dead then
+			sun.dead = true
+			for i,planet in ipairs(planets) do
+					dis = cu.dis(planet.x, planet.y, sun.x, sun.y)/400
+					sunang =  math.atan2((sun.x-planet.x),(sun.y-planet.y))-math.pi/2
+					planet.xv = planet.xv - ((sun.size*planet.size)/(dis))*math.cos(sunang)*explosionForce
+					planet.yv = planet.yv + ((sun.size*planet.size)/(dis))*math.sin(sunang)*explosionForce
+			end
+		end
+		sun.spin = sun.spin + sun.speed
 		if lk.isClick(sun.key) then
 			sun.size = sun.size + .1
+			sun.speed = sun.speed + 1
 			for i,planet in ipairs(planets) do
 				if planet.mysun.num ~= sun.num then
 					dis = cu.dis(planet.x, planet.y, sun.x, sun.y)/400
 					sunang =  math.atan2((sun.x-planet.x),(sun.y-planet.y))-math.pi/2
-					planet.xv = planet.xv + ((sun.size*planet.size)/(dis*dis))*math.cos(sunang)
-					planet.yv = planet.yv - ((sun.size*planet.size)/(dis*dis))*math.sin(sunang)
+					planet.xv = planet.xv + ((sun.size*planet.size)/(dis))*math.cos(sunang)
+					planet.yv = planet.yv - ((sun.size*planet.size)/(dis))*math.sin(sunang)
 
 				end
 			end
 		else
 			if sun.size >= 1 then
-				sun.size = sun.size-.005
+				sun.size = sun.size-.01
 			else
 				sun.size = 1
+			end
+
+
+			if sun.speed >= 1 then
+				sun.speed = sun.speed-.05
+			else
+				sun.speed = 1
 			end
 		end
 	end
 	time = time + 1
-	if time%1 == 0 and time < 20 then
+	if time%1 == 0 and time < 40 then
 		insertPlanet(1, cu.floRan(width), cu.floRan(height), cu.floRan(-5,5), cu.floRan(-5,5), cu.floRan(.5,1))
 		--insertPlanet(1, suns[1].x, suns[1].y, 0, 0, math.random(3))
 	end
@@ -193,8 +232,14 @@ end
 
 
 function g5_draw()
-	for i,sun in ipairs(suns) do
-		lg.draw(im_sun, sun.x, sun.y, 0, sun.size*sun.size*sun.lr, sun.size*sun.size, 20, 20)
+	for i,sun in ipairs(suns) do	
+		lg.setColor(255,255,255,math.min(sun.speed*20,255))
+		lg.draw(im_carona, sun.x, sun.y, sun.spin/40+math.pi/2, (2*sun.size*sun.size+sun.speed/10)*sun.lr, 2*sun.size*sun.size+sun.speed/cu.floRan(8,9), 35, 35)	
+		lg.draw(im_carona, sun.x, sun.y, sun.spin/40+math.pi/4, (2*sun.size*sun.size+sun.speed/10)*sun.lr, 2*sun.size*sun.size+sun.speed/cu.floRan(8,9), 35, 35)	
+		swapPlayerColor(sun.num,"g")
+		lg.draw(im_sun, sun.x, sun.y, 0, 2*sun.size*sun.size*sun.lr, 2*sun.size*sun.size, 20, 20)
+		unsetPlayerColor()
+
 	end
 	drawPlanets()
 end
